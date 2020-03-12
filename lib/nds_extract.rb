aeb9eaad95749f1052c95d921d8f07eb4daa87ae
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-12 21:24:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,6 @@ def gross_for_director(director_data)
   total = 0 # Total for a director's movies
   movie_index = 0 # Counter for the index of the :movies key in the d_d array
   while movie_index < director_data[:movies].count do # While the index of the director_data hash in the
-    # binding.pry
 	   total += director_data[:movies][movie_index][:worldwide_gross] # Access the gross amount for every movie and increment the total
 	   movie_index += 1 # Increment the counter
   end
@@ -20,9 +19,8 @@ def directors_totals(nds)
   directors_totals = {} # Define the final hash for the director's names and totals
   director_id = 0 # Counter variable for the current director (index)
   while director_id < nds.length do # While the current director_id (index) is less than the length of the nds do...
-    directors_totals[nds[director_id][:name]] = gross_for_director(nds[director_id]) # Setting the director name as the key and the gross
-    #total as the value for the current director nds inside the final hash directors_totals (director_id) = current director
-
+    directors_totals[nds[director_id][:name]] = gross_for_director(nds[director_id]) # Setting the current director's name as the key and
+    #the current director's gross total as the value for that key inside the final hash directors_totals
     director_id += 1 # Incrementor
   end
   directors_totals
